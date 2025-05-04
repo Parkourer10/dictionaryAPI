98c78f66f8c090c927f87071597b81ee7aec4909
Parkourer10 <fondersdev@gmail.com>
2025-05-04 11:52:03 +0000

    e

--- api.py
+++ api.py
@@ -3,10 +3,16 @@ from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
 from dictionary import dictionary
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 import json
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 @app.get("/")
 def root(request: Request):
@@ -14,7 +20,8 @@ def root(request: Request):
 
 
 @app.get("/dictionary/{word}")
-def get_dictionary(word: str):
+@limiter.limit("500/minute") #500 reqs per min for an ip
+def get_dictionary(word: str, request: Request):
     word = word.lower()
     result = dictionary(word)
     return JSONResponse(content=json.loads(result))
